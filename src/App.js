f2538fef506b82b7f5f78c58f4fffdfa23d921a4
pjmantoss <joelptoss@gmail.com>
2020-04-24 13:20:25 +0100

    update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,18 @@ import React, { Component } from 'react'
 import './App.css'
 
 export class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      brkLength: 5,
+      sessionLen: 25,
+      timerState: "stopped",
+      timerType: "Session",
+      timer: 1500,
+      intervalID: '',
+      alarmColor: { color: 'white' }
+    }
+  }
   render() {
     return (
       <div>
